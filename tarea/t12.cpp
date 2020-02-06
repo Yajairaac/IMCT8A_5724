4468b2e5c4083210d9c0733a2c1cc2f8601fba41
Yajaira Ac <yajaira_18@outlook.com>
2020-02-06 16:41:37 -0600

    actividades

---
+++ tarea/t12.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+unsigned int RES, P, IP, X;
+float PROM;
+}
---
+++ tarea/t37.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+unsigned int C, F, SP;
+}
+
